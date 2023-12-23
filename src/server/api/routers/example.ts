91ed02d9030daa48bad336132466b058e9e904e7
Burhan Syed <busyed98@gmail.com>
2023-12-23 16:24:20 -0500

    include example

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -6,7 +6,7 @@ import {
   publicProcedure,
 } from "@/server/api/trpc";
 
-export const postRouter = createTRPCRouter({
+export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
@@ -16,14 +16,15 @@ export const postRouter = createTRPCRouter({
     }),
 
   create: protectedProcedure
-    .input(z.object({ name: z.string().min(1) }))
+    .input(z.object({ content: z.string().min(1), groupId: z.string().optional() }))
     .mutation(async ({ ctx, input }) => {
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
       return ctx.db.post.create({
         data: {
-          name: input.name,
+          content: input.content,
+          group: input.groupId ? { connect: { id: input.groupId }} : undefined,
           createdBy: { connect: { id: ctx.session.user.id } },
         },
       });
