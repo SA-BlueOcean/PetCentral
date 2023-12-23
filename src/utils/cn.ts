acd0669b5bd9624a9e96e5c0d062b4cee0bc1a36
Burhan Syed <busyed98@gmail.com>
2023-12-23 16:30:00 -0500

    tw util

---
+++ src/utils/cn.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]): unknown {
+  return twMerge(clsx(inputs));
+}
