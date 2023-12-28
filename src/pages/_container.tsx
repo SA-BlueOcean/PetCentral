1f08d62072db09c0aed8f724a8d98ae75e614ff2
Ming Lee Ng <mingleeng@gmail.com>
2023-12-28 16:35:23 -0700

    add: providers to login modal
    login modal now has providers
    while signed in main webpage has a discreet signout button in bottom left corner

--- src/pages/_container.tsx
+++ src/pages/_container.tsx
@@ -1,9 +1,18 @@
 import { useGlobalContext } from "@/providers/GlobalContext";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+type Provider = {
+  id: string;
+  name: string;
+};
 
 const Container = ({ Component, pageProps }: AppProps) => {
   const { displayLoginModal, setDisplayLoginModal } = useGlobalContext();
+  const router = useRouter();
+  const { data: sessionData } = useSession();
 
   //disables scroll if modal is open
   useEffect(() => {
@@ -14,20 +23,41 @@ const Container = ({ Component, pageProps }: AppProps) => {
     }
   }, [displayLoginModal]);
 
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })().catch(console.error);
+  }, []);
+
   return (
     <>
       {displayLoginModal && (
         <div className="fixed inset-0 z-[99] bg-black/80 backdrop-blur-md">
           <div className="card absolute left-1/2 top-1/2 w-96 -translate-x-1/2 -translate-y-1/2 bg-base-100 shadow-xl">
             <div className="card-body items-center text-center">
-              <h2 className="card-title">Login!</h2>
-              <div className="card-actions justify-end">
-                <button
-                  className="btn btn-ghost"
-                  onClick={() => setDisplayLoginModal(false)}
-                >
-                  No
-                </button>
+              <h2 className="card-title">Login</h2>
+              <div className="card-actions flex flex-col items-center justify-center">
+                {Object.values(providers).map((provider: Provider) => {
+                  if (provider.name) {
+                    return (
+                      <div key={provider.name} className="flex justify-center">
+                        <button
+                          className="btn btn-primary my-1"
+                          onClick={() =>
+                            signIn(provider.id, {
+                              callbackUrl: `${window.location.origin}`,
+                            })
+                          }
+                        >
+                          Sign in with {provider.name}
+                        </button>
+                      </div>
+                    );
+                  }
+                })}
               </div>
             </div>
           </div>
@@ -39,19 +69,23 @@ const Container = ({ Component, pageProps }: AppProps) => {
         </div>
       </div>
       <div className="mx-auto flex max-w-7xl justify-center">
-        <div className="sticky top-0 hidden flex-grow self-start md:block">
-          <nav>navigation</nav>
-          <div>component a</div>
-        </div>
+        {!router.pathname.includes("auth") && (
+          <div className="sticky top-0 hidden flex-grow self-start md:block">
+            <nav>navigation</nav>
+            <div>component a</div>
+          </div>
+        )}
         <div className="relative w-full max-w-xl">
           <main className="mt-20 min-h-[calc(100vh-5rem)] bg-base-300 p-3">
             <Component {...pageProps} />
           </main>
         </div>
-        <div className="sticky top-0 hidden flex-grow self-start md:block">
-          <div>component b</div>
-          <div>component c</div>
-        </div>
+        {!router.pathname.includes("auth") && (
+          <div className="sticky top-0 hidden flex-grow self-start md:block">
+            <div>component b</div>
+            <div>component c</div>
+          </div>
+        )}
       </div>
       {/* TODO: remove this button, for demo only */}
       <button
@@ -60,6 +94,14 @@ const Container = ({ Component, pageProps }: AppProps) => {
       >
         toggle login modal
       </button>
+      {sessionData && (
+        <button
+          className="btn btn-ghost fixed bottom-0 left-0"
+          onClick={() => signOut()}
+        >
+          Sign Out
+        </button>
+      )}
     </>
   );
 };
