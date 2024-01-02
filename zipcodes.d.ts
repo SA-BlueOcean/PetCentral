2b9697e38dcd23d46eb2c18ce7974efe837d237c
Ming Lee Ng <mingleeng@gmail.com>
2024-01-02 14:55:50 -0700

    add: type definitions for zipcode

--- zipcodes.d.ts
+++ zipcodes.d.ts
@@ -1,1 +1,11 @@
-declare module 'zipcodes';
+declare module 'zipcodes' {
+    export function lookup(zip: number): Zipcode | undefined;
+
+    export interface Zipcode { 
+        zip: string; 
+        city: string; 
+        state: string; 
+        latitude: number; 
+        longitude: number; 
+    }
+}
