64849081b85d21fea75878c03c0ec0db4b5c8e7c
Derek Williams <dwilliams33dw@gmail.com>
2024-01-03 00:10:06 -0700

    made card styling similar to posts

--- src/pages/group/index.tsx
+++ src/pages/group/index.tsx
@@ -1,11 +1,12 @@
 import Head from "next/head";
-// import Link from "next/link";
+import Link from "next/link";
 import { api } from "@/utils/api";
 // import { useRouter } from "next/router";
+import Image from "next/image";
 
 export default function GroupsSearch() {
   const groupsList = api.groups.findAllGroups.useQuery();
-  console.log("+++", groupsList.data?.groups);
+
   return (
     <>
       <Head>
@@ -13,14 +14,21 @@ export default function GroupsSearch() {
         <meta name="description" content="App description" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b ">
+      <div className="flex flex-col gap-4">
         {groupsList.data?.groups.map((group) => (
-          <div key={group.id}>
-            <h1>{group.name}</h1>
-            <p>{group.description}</p>
+          <div key={group.id} className="flex ring-base-500 rounded-lg bg-base-100 ring-1 p-3">
+            <Link href={`/${group.id}`} className="relative h-10 w-10 overflow-clip rounded-full bg-secondary ring-1 ring-base-200">
+              <Image src={`${group.photoUrl}`} alt={`Image for ${group.name}`} width={50} height={50} unoptimized={true}/>
+            </Link>
+            <div className="flex flex-col mx-4">
+              <Link href={`/${group.id}`} >
+                  <h1>{group.name}</h1>
+              </Link>
+              <p>{group.description}</p>
+            </div>
           </div>
         ))}
-      </main>
+      </div>
     </>
   );
 }
