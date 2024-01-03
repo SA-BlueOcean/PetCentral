e2eb2a0ccc7e6a348988124462018bc901232e70
Burhan Syed <busyed98@gmail.com>
2024-01-03 12:58:05 -0500

    voting

--- src/components/Feed/Vote.tsx
+++ src/components/Feed/Vote.tsx
@@ -1,16 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { useGlobalContext } from "@/providers/GlobalContext";
-import { api } from "@/utils/api";
+import { api, type RouterOutputs } from "@/utils/api";
 import { cn } from "@/utils/cn";
 import { ThumbsUp, ThumbsDown } from "lucide-react";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
+import {
+  useQueryClient,
+  InfiniteData,
+} from "@tanstack/react-query";
+import { getQueryKey } from "@trpc/react-query";
 
 type VotesProps = {
   upvotes: number;
   downvotes: number;
-  vote?: number | null;
   postId: number;
+  vote?: number | null;
 };
+
+type FeedQuery = RouterOutputs["feed"]["get"];
 
 export default function Votes({
   upvotes,
@@ -19,13 +28,81 @@ export default function Votes({
   postId,
 }: VotesProps) {
   const [voteDir, setVoteDir] = useState<number | undefined>(vote ?? 0);
-  const voteMutation = api.feed.vote.useMutation();
+  const [upvoteCount, setUpvoteCount] = useState(upvotes);
+  const [downvoteCount, setDownVoteCount] = useState(downvotes);
   const session = useSession();
   const { setDisplayLoginModal } = useGlobalContext();
+  const queryClient = useQueryClient();
+  const queryKey = getQueryKey(
+    api.feed.get,
+    undefined,
+    "infinite",
+  );
+  const voteMutation = api.feed.vote.useMutation({
+    // updates the post vote data on voting
+    onSuccess: async (data) => {
+      console.log("QK?", queryKey);
+      if (!queryKey) return;
+      queryClient.setQueriesData<InfiniteData<FeedQuery>>(queryKey, (prev) => {
+        console.log("PREV?", prev);
+        const update = prev?.pages.map((page) => ({
+          ...page,
+          posts: page.posts.map((post) => {
+            if (post.id === postId) {
+              return {
+                ...post,
+                upvotes: data.post.upvotes,
+                downvotes: data.post.downvotes,
+                votes: [
+                  {
+                    id: data.vote.id,
+                    postId: data.vote.postId,
+                    userId: data.vote.userId,
+                    vote: data.vote.vote,
+                  },
+                ],
+              };
+            }
+            return post;
+          }),
+        }));
+        if (prev && update) {
+          prev.pages = update;
+        }
+        return prev;
+      });
+    },
+  });
+
   const doVote = (direction: number) => {
     if (!session.data?.user.id) {
       setDisplayLoginModal(true);
     } else {
+      // already voted, don't do anything
+      if (voteDir === direction) {
+        return;
+      }
+
+      //optimistically update the vote counts
+      if (direction === 1) {
+        setUpvoteCount((c) => c + 1);
+        if (voteDir === -1) {
+          setDownVoteCount((c) => c - 1);
+        }
+      }
+      if (direction === -1) {
+        setDownVoteCount((c) => c + 1);
+        if (voteDir === 1) {
+          setUpvoteCount((c) => c - 1);
+        }
+      }
+      if (direction === 0) {
+        if (voteDir === 1) {
+          setUpvoteCount((c) => c - 1);
+        } else if (voteDir === -1) {
+          setDownVoteCount((c) => c - 1);
+        }
+      }
       setVoteDir(direction);
       voteMutation.mutate(
         { postId, vote: direction },
@@ -34,7 +111,10 @@ export default function Votes({
             if (error.message === "UNAUTHORIZED") {
               setDisplayLoginModal(true);
             }
+            // reset optimistic updates
             setVoteDir(vote ?? 0);
+            setUpvoteCount(upvotes);
+            setDownVoteCount(downvotes);
           },
         },
       );
@@ -54,7 +134,7 @@ export default function Votes({
         }}
       >
         <ThumbsUp />
-        <span>{upvotes}</span>
+        <span>{upvoteCount}</span>
       </button>
       <button
         disabled={voteMutation.isLoading}
@@ -67,7 +147,7 @@ export default function Votes({
         }}
       >
         <ThumbsDown />
-        <span>{downvotes}</span>
+        <span>{downvoteCount}</span>
       </button>
     </div>
   );
