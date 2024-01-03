83a813c3df0b679b08193c77f80739632bda077c
derekwilliams <dwilliams33dw@gmail.com>
2024-01-03 13:43:40 -0700

    fixed duplication

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -27,25 +27,3 @@ export const appRouter = createTRPCRouter({
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
-import { createTRPCRouter } from "@/server/api/trpc";
-import { exampleRouter } from "./routers/example";
-import { feedRouter } from "./routers/feed";
-import { profileRouter } from "./routers/profile";
-import { authRouter } from "./routers/auth";
-import { groupRouter } from "./routers/groups";
-
-/**
- * This is the primary router for your server.
- *
- * All routers added in /api/routers should be manually added here.
- */
-export const appRouter = createTRPCRouter({
-  example: exampleRouter,
-  feed: feedRouter,
-  profile: profileRouter,
-  auth: authRouter,
-  groups: groupRouter,
-});
-
-// export type definition of API
-export type AppRouter = typeof appRouter;
