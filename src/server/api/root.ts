36659975a09e0d6121a3189c7838d9081d5a28b2
derekwilliams <dwilliams33dw@gmail.com>
2024-01-03 12:41:25 -0700

    merging

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -3,7 +3,6 @@ import { exampleRouter } from "./routers/example";
 import { feedRouter } from "./routers/feed";
 import { profileRouter } from "./routers/profile";
 import { authRouter } from "./routers/auth";
-import { groupRouter } from "./routers/groups";
 import { commentRouter } from "./routers/comments";
 
 /**
@@ -16,6 +15,29 @@ export const appRouter = createTRPCRouter({
   feed: feedRouter,
   profile: profileRouter,
   auth: authRouter,
+  comments: commentRouter
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
+import { createTRPCRouter } from "@/server/api/trpc";
+import { exampleRouter } from "./routers/example";
+import { feedRouter } from "./routers/feed";
+import { profileRouter } from "./routers/profile";
+import { authRouter } from "./routers/auth";
+import { groupRouter } from "./routers/groups";
+
+/**
+ * This is the primary router for your server.
+ *
+ * All routers added in /api/routers should be manually added here.
+ */
+export const appRouter = createTRPCRouter({
+  example: exampleRouter,
+  feed: feedRouter,
+  profile: profileRouter,
+  auth: authRouter,
+  groups: groupRouter,
 });
 
 // export type definition of API
