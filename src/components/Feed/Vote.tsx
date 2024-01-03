a865b8b051abe4d2f1f4bf3844c32fa5ec69af5d
Burhan Syed <busyed98@gmail.com>
2024-01-03 18:08:17 -0500

    adjust lint rules

--- src/components/Feed/Vote.tsx
+++ src/components/Feed/Vote.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { useGlobalContext } from "@/providers/GlobalContext";
 import { api, type RouterOutputs } from "@/utils/api";
 import { cn } from "@/utils/cn";
