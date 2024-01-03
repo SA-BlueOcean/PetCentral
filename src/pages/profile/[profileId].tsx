b302e1fdc6ae312694d710165bec9e34884d1ec2
Thomas Harbert <tgharbert3@gmail.com>
2024-01-03 11:15:47 -0600

    minor fix

--- src/pages/profile/[profileId].tsx
+++ src/pages/profile/[profileId].tsx
@@ -1,4 +1,4 @@
-import { api } from "@/utils/api";
+// import { api } from "@/utils/api";
 import { useRouter } from "next/router";
 import { ProfileHeader } from "@/components/Profile/ProfileHeader";
 import EditProfileModal from "@/components/Profile/EditProfileModal";
