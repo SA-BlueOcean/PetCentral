5953c3918210d7f9d3358253652fb67b341d2bc6
Burhan Syed <busyed98@gmail.com>
2024-01-02 22:18:54 -0500

    side nav auth state

--- src/components/Nav/SideNav.tsx
+++ src/components/Nav/SideNav.tsx
@@ -1,9 +1,10 @@
 import { useGlobalContext } from "@/providers/GlobalContext";
 import { api } from "@/utils/api";
 import { cn } from "@/utils/cn";
-import { FileSearch, UserRoundSearch } from "lucide-react";
+import { FileSearch, LogIn, UserRoundSearch } from "lucide-react";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
+import Avatar from "../Feed/Avatar";
 
 const links = [
   {
@@ -40,40 +41,67 @@ export default function SideNav() {
       </Link>
       <ul className="w-full divide-y">
         {links.map((link) => (
-          <li key={link.name} className="py-2 ">
-            <Link href={link.href} className="flex items-center gap-2">
+          <li key={link.name} className="py-3">
+            <Link href={link.name === "My Profile" ? `/profile/${profile.data?.id}` : link.href} className="flex items-center gap-2">
               {link.name === "My Profile" ? (
-                <div className="bg-base-400 relative h-7 w-7 overflow-clip rounded-full"></div>
+                <>
+                  {session.status === "unauthenticated" ? (
+                    <>
+                      <LogIn />
+                      <button className="">Sign in</button>
+                    </>
+                  ) : (
+                    <>
+                      <div
+                        className={cn(
+                          "bg-base-400 relative h-6 w-6 overflow-clip rounded-full",
+                          profile.isLoading && "skeleton",
+                        )}
+                      >
+                        {profile.isFetched && (
+                          <Avatar
+                            profilePhotoUrl={profile.data?.profilePhotoUrl}
+                          />
+                        )}
+                      </div>
+                      {session.status === "authenticated" ? <>{link.name}</> : ""}
+                    </>
+                  )}
+                </>
               ) : (
-                <>{link.icon}</>
+                <>
+                  {link.icon}
+                  {link.name}
+                </>
               )}
-              {link.name}
             </Link>
           </li>
         ))}
       </ul>
-      <button
-        className={cn(
-          session.status === "loading" ? "" : "",
-          "btn mt-2 h-8 min-h-8 w-full rounded-full",
-        )}
-        onClick={() => {
-          if (session.status === "authenticated") {
-            void signOut();
-          } else {
-            setDisplayLoginModal(true);
-          }
-        }}
-      >
-        {session.status === "authenticated"
-          ? "Sign out"
-          : session.status === "unauthenticated"
-            ? "Sign in"
-            : ""}
-      </button>
-      <button className="btn mt-2 h-8 min-h-8 w-full rounded-full">
-        New Post
-      </button>
+      <div className="mt-2 flex w-full flex-col gap-2">
+        <button
+          className={cn(
+            session.status === "loading" ? "" : "",
+            "btn mt-2 h-8 min-h-8 w-full rounded-full",
+          )}
+          onClick={() => {
+            if (session.status === "authenticated") {
+              void signOut();
+            } else {
+              setDisplayLoginModal(true);
+            }
+          }}
+        >
+          {session.status === "authenticated"
+            ? "Sign out"
+            : session.status === "unauthenticated"
+              ? "Sign in"
+              : ""}
+        </button>
+        <button className="btn mt-2 h-8 min-h-8 w-full rounded-full">
+          New Post
+        </button>
+      </div>
     </nav>
   );
 }
