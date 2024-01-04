adf84365daae88ee21318170e58518f5a361f881
malloryporche <malloryburke@gmail.com>
2024-01-04 12:05:27 -0800

    Updated Group Side navigation

--- src/components/Group/SideNavGroups.tsx
+++ src/components/Group/SideNavGroups.tsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
 import GroupsElements from "./GroupsElements";
 
-
-
 export default function SideNavGroups() {
   return (
-    <nav className="flex flex-col items-center justify-center gap-2 rounded-lg bg-primary-content p-4 text-neutral">
-      <Link
-        href={"/group"}
-        className="flex flex-col items-center justify-center gap-2">
-        <h2 className="text-2xl">Groups</h2>
-      </Link>
+    <nav className="relative flex max-h-[50vh] flex-col items-center justify-center gap-2 rounded-lg bg-primary-content px-4 text-neutral">
+      <div className="sticky top-0 z-50 w-full bg-primary-content pt-4">
+        <Link
+          href={"/group"}
+          className="flex flex-col items-center justify-center gap-2"
+        >
+          <h2 className="text-2xl">Groups</h2>
+        </Link>
+      </div>
       <GroupsElements />
     </nav>
   );
