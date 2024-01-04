a78434dcfe900b927c81fc32bbc1afa56516ae0a
malloryporche <malloryburke@gmail.com>
2024-01-03 20:10:15 -0800

    Added Upload functionality

--- src/components/Feed/CreatePost.tsx
+++ src/components/Feed/CreatePost.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { useGlobalContext } from "@/providers/GlobalContext";
 import { api } from "@/utils/api";
+import Image from "next/image";
+import { supabase } from "lib/supabase";
+import { v4 as uuidv4 } from "uuid";
+import { env } from "@/env.js";
 import Avatar from "./Avatar";
 
 export default function CreatePost() {
@@ -8,8 +12,10 @@ export default function CreatePost() {
     content: "",
     groupId: "",
   });
+  const [image, setImage] = useState<File | null>(null);
 
   const mutation = api.posts.createPost.useMutation({});
+  const photoMutation = api.posts.addPhoto.useMutation({});
   const { setDisplayLoginModal } = useGlobalContext();
 
   // Fetch User Details & Session Info
@@ -29,7 +35,11 @@ export default function CreatePost() {
           setDisplayLoginModal(true);
         }
       },
-      onSuccess() {
+      onSuccess(data: Number | any) {
+        const postId = data.postId;
+        if (image) {
+          getUrl(image, postId);
+        }
         setPost({
           content: "",
           groupId: "",
@@ -37,6 +47,30 @@ export default function CreatePost() {
         void utils.feed.get.invalidate();
       },
     });
+  };
+
+  const getUrl = async (file: File | null, postId: Number | any) => {
+    const filename = `${uuidv4()}`;
+    const address = `${env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/images/${filename}`;
+    const { data, error } = await supabase.storage
+      .from("images")
+      .upload(filename, file, {
+        upsert: true,
+      });
+    handleSubmitImage(postId, address);
+    return address;
+  };
+
+  const handleSubmitImage = (postId: number, photoUrl: string) => {
+    console.log(photoUrl);
+    photoMutation.mutate(
+      { postId: postId, photoUrl: photoUrl },
+      {
+        onSuccess() {
+          console.log("success");
+        },
+      },
+    );
   };
 
   return (
@@ -97,6 +131,10 @@ export default function CreatePost() {
                 name="file-upload"
                 type="file"
                 className="sr-only"
+                onChange={(e) => {
+                  const file = e.target.files?.[0];
+                  setImage(file);
+                }}
               />
             </label>
           </div>
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -14,12 +14,35 @@ export const postRouter = createTRPCRouter({
       }))
     .mutation(async ({ ctx, input }) => {
       const groupIdValue = input.groupId !== '' ? input.groupId : undefined;
-      await ctx.db.post.create({
+      const newPostId = await ctx.db.post.create({
         data: {
           content: input.content,
           groupId: groupIdValue,
           createdById: ctx.session.user.id,
         },
       });
+
+      return {
+        postId: newPostId?.id,
+      };
+  }),
+
+    addPhoto: protectedProcedure
+    .input(
+      z.object({
+        postId: z.number(),
+        photoUrl: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      console.log('hi');
+        return await ctx.db.photo.create({
+          data: {
+            url: input.photoUrl,
+            post: {
+              connect: { id: input.postId },
+            },
+          }
+        });
     }),
   });
