513a3f177ad72ea3e6daa272bf205105baedeedc
derekwilliams <dwilliams33dw@gmail.com>
2024-01-04 14:55:59 -0700

    made search groups router and search page

--- src/components/Nav/Search.tsx
+++ src/components/Nav/Search.tsx
@@ -1,23 +1,35 @@
 import { SearchIcon } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Search() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const router = useRouter();
+
+  const handleSearch = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    router.push(`/search?query=${encodeURIComponent(searchTerm)}`);
+  };
+
   return (
     <div>
       <search>
-        <form className="text-base text-base-content">
+        <form onSubmit={handleSearch} className="text-base text-base-content">
           <label htmlFor="search" className="sr-only">
             Search
           </label>
           <div className="join">
-            <div className="join-item btn rounded-l-full"><SearchIcon size={20} /></div>
+            <Link href='' onClick={handleSearch} className="join-item btn rounded-l-full"><SearchIcon size={20} /></Link>
           <input
             id="search"
             type="text"
             placeholder="Search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
             className="input join-item rounded-r-full focus:outline-none focus-visible:outline-none"
           />
           </div>
-
         </form>
       </search>
     </div>
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,23 +1,48 @@
-import Head from "next/head";
-import Link from "next/link";
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { api } from '@/utils/api';
+import { Link } from 'lucide-react';
+import Image from 'next/image';
 
-import { api } from "@/utils/api";
 
-
-export default function Home() {
-
+export default function Search() {
+  const router = useRouter();
+  const { query } = router;
+  const searchTerm = query.query || '';
+  const groupsResults = api.groups.searchGroups.useQuery(
+    searchTerm ? { searchTerm: searchTerm, } : { searchTerm: '' }
+  );
 
   return (
     <>
       <Head>
-        <title>Search | PetCentral</title>
+        <title>{searchTerm ? `Search results for: ${searchTerm} | PetCentral` : 'Search | PetCentral'}</title>
         <meta name="description" content="App description" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="container mx-auto">
-        Hello from Search Results
+      {searchTerm ? (
+        <div className="flex flex-col gap-4 w-full">Search results for: {searchTerm}
+        {groupsResults.data?.groups.map((group) => (
+          <div key={group.id} className="flex ring-base-500 rounded-lg bg-base-100 ring-1 p-3">
+            <Link href={`/${group.id}`} className="relative h-10 w-10 overflow-clip rounded-lg bg-secondary ring-1 ring-base-200">
+              <Image src={`${group.photoUrl}`} alt={`Image for ${group.name}`} width={50} height={50} unoptimized={true}/>
+            </Link>
+            <div className="flex flex-col mx-4">
+              <Link href={`/group/${group.id}`} >
+                  <h2>{group.name}</h2>
+              </Link>
+              <p>{group.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
+      ) : (
+        <div className="container mx-auto">
+          Please enter a search term
+        </div>
+      )}
     </>
   );
+
 }
 
