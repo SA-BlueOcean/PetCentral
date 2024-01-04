bc64e9da1774dc230db35a0bc89083073b337c05
hazelkimberly <hazelkimc@gmail.com>
2024-01-03 17:36:10 -0800

    Added age to pet display

--- src/components/Profile/PetCard.tsx
+++ src/components/Profile/PetCard.tsx
@@ -3,22 +3,41 @@ import Image from "next/image";
 interface Pet {
   firstName: string;
   lastName: string;
-  dateOfBirth: string;
+  dateOfBirth: Date;
   animalId: number;
   breedId: number;
   photoUrl: string;
 }
 
 export default function PetCard({ pet }: { pet: Pet }) {
+  const capitalize = (s: string) => s[0].toUpperCase() + s.slice(1);
+  const currentDate = new Date();
+  const birthdate = new Date(pet?.dateOfBirth);
+  const ageInMilliseconds = currentDate.getTime() - birthdate.getTime();
+  const ageInYears = currentDate.getFullYear() - birthdate.getFullYear();
+
+  const ageDisplay =
+    ageInMilliseconds < 0
+      ? "<1 year"
+      : ageInYears > 1
+        ? `${ageInYears} ${ageInYears === 1 ? "year" : "years"}`
+        : "<1 year";
+
   return (
     <div>
       <Image
-        src={pet.photoUrl ?? "https://clipart-library.com/images/BiaEg4n8T.jpg"}
+        src={
+          pet?.photoUrl
+            ? pet?.photoUrl
+            : "https://ph-files.imgix.net/75c2cda9-e2c3-4bcd-a0b1-0595daba1844.png?auto=format&fit=crop://clipart-library.com/images/BiaEg4n8T.jpg"
+        }
         alt={pet.firstName}
-        width={50}
-        height={50}
+        width={75}
+        height={75}
+        unoptimized
       ></Image>
-      <p>{pet?.firstName}</p>
+      <p>{capitalize(pet?.firstName)}</p>
+      <p>{ageDisplay}</p>
     </div>
   );
 }
