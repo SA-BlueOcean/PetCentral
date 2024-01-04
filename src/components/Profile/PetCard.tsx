d418016cb1d6769e605e1baa6ead6ddf5d251544
hazelkimberly <hazelkimc@gmail.com>
2024-01-03 18:36:35 -0800

    Displaying animal and breed name

--- src/components/Profile/PetCard.tsx
+++ src/components/Profile/PetCard.tsx
@@ -1,3 +1,4 @@
+import { api } from "@/utils/api";
 import Image from "next/image";
 
 interface Pet {
@@ -9,34 +10,41 @@ interface Pet {
 }
 
 export default function PetCard({ pet }: { pet: Pet }) {
+  const breed = api.pets.getSpecificBreed.useQuery({
+    breedId: pet?.breedId ?? 0,
+  }).data;
   const capitalize = (s: string) => s[0].toUpperCase() + s.slice(1);
   const currentDate = new Date();
-  const birthdate = new Date(pet?.dateOfBirth);
+  const birthdate = new Date(pet?.dateOfBirth ?? new Date());
   const ageInMilliseconds = currentDate.getTime() - birthdate.getTime();
   const ageInYears = currentDate.getFullYear() - birthdate.getFullYear();
-
   const ageDisplay =
     ageInMilliseconds < 0
       ? "<1 year"
       : ageInYears > 1
         ? `${ageInYears} ${ageInYears === 1 ? "year" : "years"}`
         : "<1 year";
-
   return (
-    <div>
+    <div className="flex">
       <Image
         src={
           pet?.photoUrl
             ? pet?.photoUrl
             : "https://ph-files.imgix.net/75c2cda9-e2c3-4bcd-a0b1-0595daba1844.png?auto=format&fit=crop://clipart-library.com/images/BiaEg4n8T.jpg"
         }
-        alt={pet.firstName}
+        alt={pet?.firstName}
         width={75}
         height={75}
         unoptimized
       ></Image>
-      <p>{capitalize(pet?.firstName)}</p>
-      <p>{ageDisplay}</p>
+      <div className="flex flex-col justify-between">
+        <p>{capitalize(pet?.firstName ?? "")}</p>
+        <div className="flex gap-x-12">
+          <p>{ageDisplay}</p>
+          <p>{capitalize(breed?.animal?.name ?? "")}</p>
+          <p>{capitalize(breed?.name ?? "")}</p>
+        </div>
+      </div>
     </div>
   );
 }
